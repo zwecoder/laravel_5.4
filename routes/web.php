0c277e2023ab4bd273e5f7f7ecb3f7a463c82c4c
zwecoder <zwewathone77@gmail.com>
2025-02-19 22:09:13 +0630

    second commit raw SQL test

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
+use Illuminate\Support\Facades\Route;
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -42,7 +44,41 @@ Route::get('/home','HomeController@home');
 //Route::get('/home/{name}/{country}','HomeController@ManyPara');
 //chaper 19 passsing data with array compact
 //Route::get('/home/{name}/{country}','HomeController@ManyParaWithCompact');
+
+///////chapter 29 insert data ////////////////////////////
 Route::get('/insert',function(){
     //DB::insert('insert into post(title,content) values(?,?)',['Laravel title', 'Laravel content']);
     DB::insert('insert into post(title,content) value(?,?)',['Laravel title 1','Laravel content 1']);
+});
+///////////////////////////////////////////////////
+
+/////////////chapter 30 read data ////////////////
+Route::get('/read',function(){
+    //     $result=DB::select('select * from post where id=?',[1]);
+    $result=DB::select('select * from post');
+    foreach($result as $post){
+       echo $post->content.'<br>'.$post->title.'<br>';
+    }
+});
+//////////////////////////////////////////////////////////////
+
+///////chapter 31 update data ////////////////////////////
+Route::get('/update',function(){
+    $result =DB::update('update post set title=? where id=?',['testing 1',1]);
+    if($result){
+        return 'Data updated successfully';
+    }else{
+        return 'Data not updated';
+    }
+});
+/////////////////////////////////////////////////
+
+///////chapter 32 delete data ////////////////////////////
+Route::get('/delete',function(){
+    $result=DB::delete('delete from post where id=?',[1]);
+    if($result){
+        return 'Data deleted successfully';
+    }else{  
+        return 'Data not deleted';
+    }   
 });
